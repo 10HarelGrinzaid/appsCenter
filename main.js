47af44b89ca19dca179b1e406bac49f84f687f8f
nadav04 <pinHeadLarry@1>
2022-08-11 11:22:32 +0300

    almost done

--- main.js
+++ main.js
@@ -12,11 +12,12 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 
-const showAppsDate = (filter) => {
-    const appsArray = getData();
+const showAppsDate = (filt) => {
 
-    console.log(appsArray);
-    const element = document.getElementById("appList");
+    const appsJASON = getData();
+
+    //console.log(appsJASON);
+    const element = document.getElementById('appList');
 
     var child = element.lastElementChild;
     while (child) {
@@ -24,57 +25,74 @@ const showAppsDate = (filter) => {
         child = element.lastElementChild;
     }
 
-    appsArray.forEach(app => {
 
+    var newAppsArray = getData().filter(function (app) {
+        return app['name'] == filt;
 
-
-        if (doesAppMachFilter(extractValueFromInput('form1'), app, 'name')) {
-
-            const divAppInfo = document.createElement("div");
-
-            const appInfo = document.createElement("ul");
-            const appInfoName = document.createElement("li");
-            const appInfoPrice = document.createElement("li");
-
-            appInfoPrice.classList.add("list-group-item");
-            appInfoPrice.classList.add("list-group-item-action");
-            appInfoPrice.classList.add("list-group-item-primary");
-
-            appInfoName.classList.add("list-group-item");
-            appInfoName.classList.add("list-group-item-action");
-            appInfoName.classList.add("list-group-item-primary");
-
-            const nameNode = document.createTextNode(app['name']);
-            const priceNode = document.createTextNode(app['price']);
-
-            var documentFragment = document.createDocumentFragment();
-
-
-            appInfoPrice.appendChild(nameNode);
-            appInfoName.appendChild(priceNode);
-
-            documentFragment.appendChild(appInfo);
-
-            appInfo.appendChild(appInfoPrice);
-            appInfo.appendChild(appInfoName);
-
-            divAppInfo.appendChild(documentFragment);
-
-
-
-            element.appendChild(divAppInfo);
-        }
+        //TODO: find a way to use regexp in here
     });
+
+
+
+    console.log(newAppsArray);
+
+    newAppsArray.forEach(app => {
+
+        const divAppInfo = document.createElement("div");
+
+        const appInfo = document.createElement("ul");
+        const appInfoName = document.createElement("li");
+        const appInfoPrice = document.createElement("li");
+
+        appInfoPrice.classList.add("list-group-item");
+        appInfoPrice.classList.add("list-group-item-action");
+        appInfoPrice.classList.add("list-group-item-primary");
+
+        appInfoName.classList.add("list-group-item");
+        appInfoName.classList.add("list-group-item-action");
+        appInfoName.classList.add("list-group-item-primary");
+
+        const nameNode = document.createTextNode(app['name']);
+        const priceNode = document.createTextNode(app['price']);
+
+        var documentFragment = document.createDocumentFragment();
+
+
+        appInfoPrice.appendChild(nameNode);
+        appInfoName.appendChild(priceNode);
+
+        documentFragment.appendChild(appInfo);
+
+        appInfo.appendChild(appInfoPrice);
+        appInfo.appendChild(appInfoName);
+
+        divAppInfo.appendChild(documentFragment);
+
+
+
+        element.appendChild(divAppInfo);
+    }
+    );
 }
 
-const doesAppMachFilter = (filter, app, filterby) => {
+const doesAppMachFilter = (filter, filterby) => {
 
     const pattern = new RegExp(filter, 'i');
     return pattern.test(app[filterby]);
 }
 
-const extractValueFromInput = (id) => {
-    const val = document.querySelector(`#${id}`).value;
-    console.log(val);
-    return val;
+function switchToAddApplication() {
+    window.location.href = "addApplication.html"
 }
+
+const filteredApps = (filt, filterby) => getData().filter(app => {
+
+    const regexp = new RegExp(filt, 'i');
+
+    let appFiltBy = app[filterby]
+
+    if (regexp.test(appFiltBy)) {
+        return app;
+    }
+});
+
